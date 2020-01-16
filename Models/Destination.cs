2e3d1ee0c971015769035daac70da6e1763b7730
Miguel Gomez <mgomez.developer@gmail.com>
2020-01-16 10:28:38 -0500

    Create Initial Migration for Destination Model

---
+++ Models/Destination.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CruiseCMSDemo.Models
+{
+    public class Destination
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public string Region { get; set; }
+        public string Country { get; set; }
+        [Required]
+        public string Place { get; set; }
+        [Required]
+        public string Motto { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public string Population { get; set; }
+        [Required]
+        public byte[] Picture { get; set; }
+
+    }
+}
