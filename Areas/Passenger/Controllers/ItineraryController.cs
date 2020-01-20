845caa9953018b1943bf7576a8935e4d88b4ea03
Miguel Gomez <mgomez.developer@gmail.com>
2020-01-20 14:30:19 -0500

    Create Controller and ViewPage for Itinerary

--- Areas/Passenger/Controllers/ItineraryController.cs
+++ Areas/Passenger/Controllers/ItineraryController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CruiseCMSDemo.Data;
+using CruiseCMSDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CruiseCMSDemo.Areas.Customer.Controllers
 {
-    [Area("Customer")]
+    [Area("Passenger")]
     public class ItineraryController : Controller
     {
         private readonly ApplicationDbContext _db;
@@ -21,6 +22,26 @@ namespace CruiseCMSDemo.Areas.Customer.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _db.Itinerary.ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Itinerary itinerary)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Itinerary.Add(itinerary);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(itinerary);
         }
 
 
--- Startup.cs
+++ Startup.cs
@@ -64,7 +64,7 @@ namespace CruiseCMSDemo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{area=customer}/{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
