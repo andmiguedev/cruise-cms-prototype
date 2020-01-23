ef9ba568a641e1267a97ab206b539418bb0ad13f
Miguel Gomez <mgomez.developer@gmail.com>
2020-01-23 16:34:37 -0500

    Create ViewModel to render Destination List

---
+++ ViewModel/PassengerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CruiseCMSDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CruiseCMSDemo.ViewModel
+{
+    public class PassengerViewModel
+    {
+        public IEnumerable<Itinerary> SelectDestination { get; set; }
+        public Profile Profile { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
