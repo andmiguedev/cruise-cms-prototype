0f5e3ef55070bc442c75eac2a2437b21d73d2e53
Miguel Gomez <mgomez.developer@gmail.com>
2020-02-05 10:58:08 -0500

    Create Controller and ViewPage for Admin

---
+++ Areas/Manager/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using CruiseCMSDemo.Data;
+
+namespace CruiseCMSDemo.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public AdminController(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        /**
+         * Render a list of selected Administrators. One 
+         * of them is in charge of updating the banner 
+         * image and description on the Home page.
+         */ 
+        public async Task<IActionResult> Index()
+        {
+            var webmaster = await _db.Administrator.ToListAsync();
+
+            return View(webmaster);
+        }
+    }
+}
