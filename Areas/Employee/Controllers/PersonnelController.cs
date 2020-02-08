0357c65f4c2ef64910ac4679e59b06932f4b8a6e
Miguel Gomez <mgomez.developer@gmail.com>
2020-02-08 00:32:49 -0500

    Setup Index Controller and ViewPage for Staff

---
+++ Areas/Employee/Controllers/PersonnelController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CruiseCMSDemo.Data;
+using CruiseCMSDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CruiseCMSDemo.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class PersonnelController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public PersonnelController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        /**
+         * Render a list of important information for
+         * Cruise employees that managers keep track
+         */
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Personnel.ToListAsync());
+        }
+    }
+}
